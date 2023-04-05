36bcd51017f91c587176d5de6b1edc5c2006c60c
Roman <roman-right@protonmail.com>
2023-04-05 23:09:39 +0200

    update | GH action | publish_docs | working directory | 8

--- docs/publish.sh
+++ docs/publish.sh
@@ -1,6 +1,3 @@
-poetry run pydoc-markdown
-cd build
-
 remote_repo="https://x-access-token:${GITHUB_TOKEN}@${GITHUB_DOMAIN:-"github.com"}/${GITHUB_REPOSITORY}.git"
 git remote rm origin
 git remote add origin "${remote_repo}"
